f432858b2c492d8afdcd36781a9de5005fc4eccc
Thant Yar Zar Hein <kothantyarzarhein@gmail.com>
2019-01-30 21:00:01 +0630

    Initial commit

---
+++ 3-StringManipulation2.js
@@ -0,0 +1,31 @@
+
+    /*
+      *
+    * Use string methods split(), slice(), toLowerCase() and toUpperCase() 
+    * and array methods join()
+    *  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+    *  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+    */
+
+    const name = "AlbERt eINstEiN";
+
+    function nameFormatter(oldName) {      
+      /* 
+        Split the string by space
+        Slice the first character and make it uppercase
+        Slice the reset of the characters and make it lowercase
+        Join the splitted array with space to get the final result
+        Final result should be "Albert Einstein"                
+        */
+
+        var str = oldName;
+
+        var res = str.split(" ");
+
+        const firstName = res[0].charAt(0).toUpperCase() + res[0].substr(1).toLowerCase();
+        const LastName = res[1].charAt(0).toUpperCase() + res[1].substr(1).toLowerCase();
+
+        const name = firstName + " " + LastName;
+        return name;
+    }
+    console.log(nameFormatter(name));
